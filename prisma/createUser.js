2f14cfd44b5b2553fab1b7a3af7ae1888461f004
Jetmir <Ahmati>
2025-07-10 14:56:57 +0200

    more changes

---
+++ prisma/createUser.js
@@ -0,0 +1,36 @@
+// prisma/seed.ts
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const email = 'emiliano.duraku@dela-tech.com';
+  const existing = await prisma.user.findUnique({ where: { email } });
+
+  if (existing) {
+    console.log('🚫 Default admin already exists.');
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash('Admin123.', 10);
+  const user = await prisma.user.create({
+    data: {
+      username: 'admin',
+      email,
+      password: hashedPassword,
+      role: 'admin',
+    },
+  });
+
+  console.log(' Default admin user created:', user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
