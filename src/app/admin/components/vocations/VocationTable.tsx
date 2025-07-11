9e755d5a5c325f6de9175004b1f7898a373e0726
Emiliano Duraku <emiliano.duraku@dela-tech.com>
2025-07-11 12:43:32 +0200

    test commit

--- src/app/admin/components/vocations/VocationTable.tsx
+++ src/app/admin/components/vocations/VocationTable.tsx
@@ -27,7 +27,7 @@ export default function VocationTable({
           <thead>
             <tr>
               <th colSpan={5} className="text-[#244B77] text-center py-4 text-xl">
-                Nuk ka pushime.
+                Nuk ka pushime shto pushimet.
               </th>
             </tr>
           </thead>
