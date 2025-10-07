c0668fb7f2600b4cdf3d85197e9437617a2714c0
Relando Vrapi <relando.vrapi@dela-tech.com>
2025-10-07 14:08:38 +0200

    re-enabled double render in development mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
   experimental: {
     optimizeCss: false, // 🔧 disables LightningCSS to avoid native module errors
   },
