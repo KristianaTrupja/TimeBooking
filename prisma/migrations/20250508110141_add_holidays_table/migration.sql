f7e9bd1255be6893f59977518a01c75b0faaf1fb
Kristiana <k.trupja@reply.de>
2026-01-08 16:02:33 +0100

    Revert "updates"
    This reverts commit 4ea548818f6c7bc93cbd6493b84f19d4c0352370.

--- prisma/migrations/20250508110141_add_holidays_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Holidays" (
-    "id" SERIAL NOT NULL,
-    "date" TEXT NOT NULL,
-    "holiday" TEXT NOT NULL,
-
-    CONSTRAINT "Holidays_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20250508134411_add_project_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Projects" (
-    "id" SERIAL NOT NULL,
-    "company" TEXT NOT NULL,
-    "project" TEXT NOT NULL,
-
-    CONSTRAINT "Projects_pkey" PRIMARY KEY ("id")
-);
