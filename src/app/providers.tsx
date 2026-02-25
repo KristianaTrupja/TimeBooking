d6e91f15b9179d91b7a8f05e03fa8ea10331ef15
delatech-git <159771494+delatech-git@users.noreply.github.com>
2026-02-25 15:50:14 +0100

    deploy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,9 @@
 import { SessionProvider } from "next-auth/react";
 import type { ReactNode } from "react";
 
+
+
+
 export function Providers({ children }: { children: ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
